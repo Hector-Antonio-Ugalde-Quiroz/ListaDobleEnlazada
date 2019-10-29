1a9220c899294cb662b0f0f9262a723dc62e688f
Hector-Antonio-Ugalde-Quiroz <54918207+Hector-Antonio-Ugalde-Quiroz@users.noreply.github.com>
2019-10-28 20:03:10 -0600

    Add files via upload
    Aqui se presentan los nuevos cambios del documento que fueron modificados

--- Lista Doblemente Circulares/src/lista/doblemente/circulares/ListaDoblementeCirculares.java
+++ Lista Doblemente Circulares/src/lista/doblemente/circulares/ListaDoblementeCirculares.java
@@ -5,6 +5,8 @@
  */
 package lista.doblemente.circulares;
 
+import java.util.Scanner;
+
 /**
  *
  * @author heuga
@@ -18,22 +20,43 @@ public class ListaDoblementeCirculares {
         // TODO code application logic here
 
         ListaCircular x = new ListaCircular();
-        int a = 5;
-        int b = 10;
-        int c = 15;
-        int d = 20;
-        int e = 25;
-        int f = 30;
-        x.alta(a);
-        x.alta(b);
-        x.alta(c);
-        x.alta(d);
-        x.alta(e);
-        x.alta(f);
-        System.out.println("Valores iniciales:\n");
-        x.imprimir();
-        x.bajaxposicion(0);
-        System.out.println("\nSe acaba de borrar un numero\n");
-        x.imprimir();
+        int opcion;
+       do {
+            System.out.println("Que desea hacer");
+            System.out.println("1. Ingresar datos a la lista");
+            System.out.println("2. Imprimir lista");
+            System.out.println("3. Eliminar datos");
+            System.out.println("4. salir");
+            opcion = leerentero("Seleccione una opcion");
+
+            switch (opcion) {
+                case 1: {
+                    int nuevo = leerentero("ingrese el numero");
+                    x.agregarAlFinal(nuevo);
+                    break;
+                }
+                case 2: {
+                    System.out.println("La lista es: ");
+                    x.listar();
+                    break;
+                }
+                case 3: {
+                    int valor = leerentero("¿Cual desea eliminar?");
+                    x.eliminar();
+                    break;
+                }
+                case 4: {
+                    System.out.println("Hasta luego");
+                    break;
+                }
+            }
+        } while (opcion != 4);
+    }
+
+    public static int leerentero(String texto) {
+ Scanner leer = new Scanner(System.in);
+        System.out.println(texto);
+        int dato = leer.nextInt();
+        return dato;
     }
 }
